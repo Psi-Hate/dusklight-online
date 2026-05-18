fdcff0736da00fab13b43f04ff1fdaae31a30268
Psi-Hate <Psi-Hate@users.noreply.github.com>
2026-05-18 15:07:36 -0500

    change to pre-hooks, disable og draw/execute

--- src/mod.cpp
+++ src/mod.cpp
@@ -4,27 +4,29 @@
 #include "d/actor/d_a_alink.h"
 #include "d/actor/d_a_warp_bug.h"
 
-
 extern "C" {
 
 static DuskModAPI* gApi;
 static daWarpBug_c* puppet;
 static daAlink_c* link;
 
-static void on_create_post(void* ret, void* args) {
+static int32_t on_create_pre(void* args) {
+    puppet = dusk::arg<daWarpBug_c*>(args, 0);
     link = daAlink_getAlinkActorClass();
-    puppet = reinterpret_cast<daWarpBug_c*>(ret);
-    gApi->log_info("puppet init: %p", puppet);
+    gApi->log_info("puppet create: %p", puppet);
+    return 0;
 }
 
-static void on_execute_post(void* ret, void* args) {
+static int32_t on_execute_pre(void* args) {
     puppet->current.pos = link->current.pos;
     gApi->log_info("puppet ontick: %f", puppet->current.pos.x);
+    return 1;
 }
 
-static void on_draw_post(void* ret, void* args) {
+static int32_t on_draw_pre(void* args) {
     puppet->current.pos = link->current.pos;
     gApi->log_info("puppet draw: %f", puppet->current.pos.y);
+    return 1;
 }
 
 void mod_init(DuskModAPI* api) {
@@ -34,9 +36,9 @@ void mod_init(DuskModAPI* api) {
     api->log_info("Dusklight-Online initialized.");
 
     // Setup puppet hooks
-    dusk::hookAddPost<&daWarpBug_c::create>(on_create_post);
-    dusk::hookAddPost<&daWarpBug_c::execute>(on_execute_post);
-    dusk::hookAddPost<&daWarpBug_c::draw>(on_draw_post);
+    dusk::hookAddPre<&daWarpBug_c::create>(on_create_pre);
+    dusk::hookAddPre<&daWarpBug_c::execute>(on_execute_pre);
+    dusk::hookAddPre<&daWarpBug_c::draw>(on_draw_pre);
 }
 
 void mod_tick(DuskModAPI* api) {
